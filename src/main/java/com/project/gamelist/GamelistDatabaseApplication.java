b59b7a101214eed9d690390b7d6674ac74a8a704
Ranieeery <raniery2003@hotmail.com>
2023-05-19 04:47:49 -0300

    feat: create project

---
+++ src/main/java/com/project/gamelist/GamelistDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelistDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/gamelist/GamelistDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
